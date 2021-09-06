70fd23bf3f08185d55fc3bb79627252bbb513d18
jackra1n <lok7ok@gmail.com>
2021-09-07 01:18:08 +0200

    something

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,57 @@
-use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io;
+use text_io::read;
 
 
-// fn get_text_menu(text_menu: &str) -> Result<String, std::io::Error> {
-fn get_text_menu(text_menu: &str) -> String {
-    let mut path = "/resources/text_menus/".to_owned();
-    path.push_str(text_menu);
+fn get_menu(menu: &str) -> String {
+    let mut path = "resources/text_menus/".to_owned();
+    path.push_str(menu);
     path.push_str(".txt");
-    // println!("result: {}", main_path);
-    return path;
-    // return fs::read_to_string();
+    let mut file = File::open(path).expect("Unable to open the file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Unable to read the file");
+    return contents;
+}
+
+fn print_menu(menu: &str) {
+    print!("{}", get_menu(menu));
+}
+
+fn print_menu_choose() {
+    print!("{}", get_menu("MainMenu"));
+    let input: i32 = read!();
+    menu_choose(input)
+}
+
+fn menu_choose(option: i32) {
+    if option == 1 {
+        start_game();
+    }
+    else if option == 2 {
+        print_menu("Instructions");
+    }
+    else if option == 3 {
+        print_menu("Credits");
+    }
+    else if option == 4 {
+        print_menu("ClassStats")
+    }
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    
+    print!("it should wait before this");
+    print_menu_choose();
+}
+
+fn start_game() {
+    print!("Name the first warrior: ");
 }
 
 fn main() {
-    // print_main_menu();
-    get_text_menu("MainMenu");
-}
-
-fn print_main_menu() {
-    println!("Main");
+    print_menu("HomeScreen");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    print_menu_choose();
 }
