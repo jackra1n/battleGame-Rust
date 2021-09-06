43e46fae2519e6ef679a8566552a8260ba2f8bee
jackra1n <lok7ok@gmail.com>
2021-09-06 23:44:13 +0200

    damn this is hard...

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use std::fs;
+
+
+// fn get_text_menu(text_menu: &str) -> Result<String, std::io::Error> {
+fn get_text_menu(text_menu: &str) -> String {
+    let mut path = "/resources/text_menus/".to_owned();
+    path.push_str(text_menu);
+    path.push_str(".txt");
+    // println!("result: {}", main_path);
+    return path;
+    // return fs::read_to_string();
+}
+
 fn main() {
-    print_main_menu();
+    // print_main_menu();
+    get_text_menu("MainMenu");
 }
 
 fn print_main_menu() {
-    println!("What do you want to do?");
-    println!("Start Game............[1]");
+    println!("Main");
 }
